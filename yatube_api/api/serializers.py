b376f49993330107d2af0d85cdc01d6126a005a1
Svyatoslav Vinogradov <vinogradov.s94@yandex.ru>
2023-01-19 18:30:17 +0300

    First commit

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -51,7 +51,7 @@ class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
     )
-    
+
     class Meta:
         fields = '__all__'
         model = Comment
